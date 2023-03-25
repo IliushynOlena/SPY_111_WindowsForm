a05e4375a60045f15a1805392b7c6755dcbce603
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-03-25 12:03:31 +0200

    GraphicsGDI

---
+++ 13_GraphicsGDI/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace _13_GraphicsGDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawBtn = new System.Windows.Forms.Button();
+            this.colorCb = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // DrawBtn
+            // 
+            this.DrawBtn.Location = new System.Drawing.Point(1361, 89);
+            this.DrawBtn.Name = "DrawBtn";
+            this.DrawBtn.Size = new System.Drawing.Size(321, 129);
+            this.DrawBtn.TabIndex = 0;
+            this.DrawBtn.Text = "Draw";
+            this.DrawBtn.UseVisualStyleBackColor = true;
+            this.DrawBtn.Click += new System.EventHandler(this.DrawBtn_Click);
+            // 
+            // colorCb
+            // 
+            this.colorCb.FormattingEnabled = true;
+            this.colorCb.Location = new System.Drawing.Point(5, 8);
+            this.colorCb.Name = "colorCb";
+            this.colorCb.Size = new System.Drawing.Size(350, 49);
+            this.colorCb.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(395, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(376, 49);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1827, 915);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.colorCb);
+            this.Controls.Add(this.DrawBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawBtn;
+        private System.Windows.Forms.ComboBox colorCb;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ 13_GraphicsGDI/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_GraphicsGDI
+{
+    public partial class Form1 : Form
+    {
+        enum ShapeType { Rectangle, Line , Ellipse};
+        int redraw_count = 0;
+        List<Point> points = new List<Point>();
+        List<MyShape> shapes = new List<MyShape>();
+        Point startLocation;
+        public Form1()
+        {
+            InitializeComponent();
+            colorCb.Items.Add(Color.Gray);
+            colorCb.Items.Add(Color.Green);
+            colorCb.Items.Add(Color.Red);
+            colorCb.Items.Add(Color.Yellow);
+            colorCb.Items.Add(Color.Chartreuse);
+            colorCb.SelectedIndex = 0;
+            foreach (var item in Enum.GetValues(typeof(ShapeType)))
+            {
+                comboBox1.Items.Add(item);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void DrawBtn_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            int x = 33;
+            int y = 33;
+            int width = 300;
+            int height = 300;
+            Pen pen = new Pen(Brushes.Yellow, 5);
+            Point point = new Point(10, 10);
+            Size size = new Size(350, 350);
+
+            Rectangle rectangle1  =new Rectangle(point, size);
+
+            Rectangle rectangle2 = new Rectangle(x,y,width,height);
+            g.DrawEllipse(pen, rectangle1);
+            g.FillEllipse(Brushes.Blue, rectangle2);    
+        }
+        private void PrintPoints(Graphics g)
+        {
+            foreach (Point p in points)
+            {
+                g.FillEllipse(Brushes.Teal, p.X - 10, p.Y - 10, 20, 20);
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //PrintPoints(e.Graphics);
+            //Exapmle 1
+            //this.Text = $"{redraw_count++}";
+            //Graphics g = e.Graphics;
+            //int x = 33;
+            //int y = 33;
+            //int width = 300;
+            //int height = 300;
+            //Pen pen = new Pen(Brushes.Yellow, 5);
+            //Point point = new Point(10, 10);
+            //Size size = new Size(350, 350);
+
+            //Rectangle rectangle1 = new Rectangle(point, size);
+
+            //Rectangle rectangle2 = new Rectangle(x, y, width, height);
+            //g.DrawEllipse(pen, rectangle1);
+            //g.FillEllipse(Brushes.Blue, rectangle2);
+
+            //Exapmle 2
+            //using (Graphics g = e.Graphics)
+            //{
+            //    Pen pen = new Pen(Color.Green, 20);
+            //    pen.StartCap = LineCap.SquareAnchor;
+            //    pen.EndCap = LineCap.ArrowAnchor;
+            //    pen.DashStyle = DashStyle.DashDot;  
+            //    pen.DashCap = DashCap.Triangle;
+
+            //    g.DrawLine(pen, 20, 200, 900, 200);
+
+            //    Pen p = new Pen(Brushes.Red, 8);
+            //    p.DashStyle = DashStyle.Dash;
+            //    Rectangle rectangle = new Rectangle(33, 33, 500, 500);
+            //    g.DrawEllipse(p, rectangle);
+
+            //    Rectangle rectangle1 = new Rectangle(60, 60, 300, 300);
+            //    g.FillEllipse(Brushes.Brown, rectangle1);
+
+            //}//Dispose();
+
+            ///example 3
+             Graphics g =  e.Graphics;
+            foreach (var shape in shapes)
+            {
+                shape.Draw(g);
+            }
+
+            Font f = new Font("Verdana", 15, FontStyle.Bold | FontStyle.Italic);
+            g.DrawString("Hello world ", f, Brushes.Black, 50, 50);
+
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            //Graphics g = e.Graphics;
+        }     
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+        //    Point point = e.Location;
+        //    points.Add(point);
+        //    this.CreateGraphics().FillEllipse(Brushes.Teal, point.X-10, point.Y-10, 20, 20);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Text = "Down";
+            startLocation = e.Location;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+
+            this.Text = "Up";
+            Point end = e.Location;
+            Graphics g = this.CreateGraphics();
+            var type = (ShapeType)comboBox1.SelectedItem;
+            Color color = (Color)colorCb.SelectedItem;
+            MyShape shape = null;
+            if (type == ShapeType.Rectangle || type == ShapeType.Ellipse)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.X = startLocation.X < end.X ? startLocation.X : end.X;
+                rectangle.Y = startLocation.Y < end.Y ? startLocation.Y : end.Y;
+
+                rectangle.Width = Math.Abs(e.X - startLocation.X);
+                rectangle.Height = Math.Abs(e.Y - startLocation.Y);
+
+                if(type == ShapeType.Rectangle)
+                {
+                     shape = new MyRectangle(color, rectangle);          
+                }
+                else
+                {
+                    shape = new MyEllipse(color, rectangle);
+                }
+            }
+            else if(type == ShapeType.Line)
+            {
+                shape = new MyLine(color, startLocation, end);               
+            }
+
+            shape.Draw(g);
+            shapes.Add(shape);
+        }
+    }
+    abstract class MyShape
+    {
+        public abstract void Draw(Graphics g);
+    }
+    class MyRectangle :MyShape
+    {
+        //public Color Color { get; set; }
+        public Brush Brush { get; set; }
+        public Rectangle Rectangle { get; set; }
+        public MyRectangle()  {     }
+        public MyRectangle(Color Color, Rectangle Rectangle)
+        {
+            this.Brush = new SolidBrush(Color); 
+            this.Rectangle = Rectangle;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.FillRectangle(Brush, Rectangle);  
+        }
+    }
+    class MyLine : MyShape
+    {
+        public Pen Pen { get; set; }
+        public Point Start { get; set; }
+        public Point End { get; set; }
+        public MyLine()   {     }
+        public MyLine(Color c, Point s, Point e)
+        {
+            Pen = new Pen(c);
+            Start = s;
+            End = e;
+        }
+        public override void Draw(Graphics g)
+        {
+            g.DrawLine(Pen, Start, End);
+        }
+    }
+    class MyEllipse :MyRectangle
+    {
+        public MyEllipse() {  }
+        public MyEllipse(Color c, Rectangle r): base(c,r){}
+        public override void Draw(Graphics g)
+        {
+            g.FillEllipse(Brush, Rectangle);
+        }
+
+    }
+}
